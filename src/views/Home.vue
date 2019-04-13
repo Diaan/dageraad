f6085631779cd1f4c12cfc6a8242f99823a168d6
Diaan <info@dianabroeders.nl>
2019-04-13 09:03:44 +0200

    moved root folder

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Wheel/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Wheel from "@/components/Wheel.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Wheel
+  }
+})
+export default class Home extends Vue {}
+</script>
