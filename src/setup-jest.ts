24ce42b5440bdf8e705ffe1edab5862e97d4416e
Diana Broeders <diana.broeders@sanoma.com>
2019-07-27 11:54:06 +0200

    redirects + switch to jest

---
+++ src/setup-jest.ts
@@ -0,0 +1,23 @@
+import 'jest-preset-angular';
+
+const storageMock = () => {
+  let storage: any = {};
+  return {
+    getItem: (key: any) => (key in storage ? storage[key] : null),
+    setItem: (key: any, value: any) => (storage[key] = value || ''),
+    removeItem: (key: any) => delete storage[key],
+    clear: () => (storage = {})
+  };
+};
+
+Object.defineProperty(window, 'localStorage', { value: storageMock() });
+Object.defineProperty(window, 'sessionStorage', { value: storageMock() });
+Object.defineProperty(window, 'scrollTo', { value: () => { } });
+
+Object.defineProperty(window, 'matchMedia', {
+  value: () => ({
+    matches: false,
+    addListener: () => { },
+    removeListener: () => { }
+  })
+});
