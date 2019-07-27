7eb3723fbff81f0f5b25c6e381d3504ad3aba3c3
Diana Broeders <diana.broeders@sanoma.com>
2019-07-27 13:15:15 +0200

    setup complete 🤓

--- jest.config.js
+++ jest.config.js
@@ -2,47 +2,24 @@
 module.exports = {
   preset: 'jest-preset-angular',
   roots: [
-    './src/'
+    './src/app'
   ],
-  // modulePathIgnorePatterns: [
-  //   '<rootDir>/src/test.ts',
-  //   '<rootDir>/src/coverage',
-  //   '<rootDir>/coverage',
-  //   '<rootDir>/.*\.e2e\.ts'
-  // ],
-  // collectCoverageFrom: [
-  //   '**/*.{ts,tsx}',
-  //   '!**/node_modules/**',
-  //   '!**/src/setup-jest.ts',
-  //   '!**/src/**/index.ts',
-  //   '!**/src/**/*.module.ts'
-  // ],
-  // coverageReporters: [
-  //   'json',
-  //   'lcov',
-  //   'text-summary'
-  // ],
-  // coverageThreshold: {
-  //   global: {
-  //     statements: 1,
-  //     branches: 1,
-  //     functions: 1,
-  //     lines: 1
-  //   }
-  // },
-  // moduleNameMapper: {
-  //   "^@compat/(.*)": "<rootDir>/src/app/compat/$1",
-  //   "^@core/(.*)": "<rootDir>/src/app/core/$1",
-  //   "^@environments/(.*)": "src/environments/$1",
-  //   "^@features/(.*)": "<rootDir>/src/app/features/$1",
-  //   "^@shared/(.*)": "<rootDir>/src/app/shared/$1",
-  //   "^@student/(.*)": "<rootDir>/src/app/student/$1",
-  //   "^@teacher/(.*)": "<rootDir>/src/app/teacher/$1",
-  //   "^@efc-1.x/(.*)": "<rootDir>/src/efc-1.x/$1",
-  //   "^@vo-commons/(.*)": "<rootDir>/src/vo-commons/$1",
-  //   "^@ajs/(.*)": "<rootDir>/src/app-ajs/components/$1",
-  //   "^@test/(.*)": "<rootDir>test/$1"
-  // },
-  // setupFiles: ['jest-canvas-mock'],
-  // setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts']
+  modulePathIgnorePatterns: [
+    '<rootDir>/src/test.ts',
+    '<rootDir>/coverage',
+    '<rootDir>/.*\.e2e\.ts'
+  ],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/src/setup-jest.ts',
+    '!**/src/**/index.ts',
+    '!**/src/**/*.module.ts'
+  ],
+  coverageReporters: [
+    'json',
+    'lcov',
+    'text-summary'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts']
 };
--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,23 +1,25 @@
 import 'jest-preset-angular';
+// import 'global-mocks';
+// require('node_modules/@angular-builders/jest/dist/jest-config/setup.js');
 
-const storageMock = () => {
-  let storage: any = {};
-  return {
-    getItem: (key: any) => (key in storage ? storage[key] : null),
-    setItem: (key: any, value: any) => (storage[key] = value || ''),
-    removeItem: (key: any) => delete storage[key],
-    clear: () => (storage = {})
-  };
-};
+// const storageMock = () => {
+//   let storage: any = {};
+//   return {
+//     getItem: (key: any) => (key in storage ? storage[key] : null),
+//     setItem: (key: any, value: any) => (storage[key] = value || ''),
+//     removeItem: (key: any) => delete storage[key],
+//     clear: () => (storage = {})
+//   };
+// };
 
-Object.defineProperty(window, 'localStorage', { value: storageMock() });
-Object.defineProperty(window, 'sessionStorage', { value: storageMock() });
-Object.defineProperty(window, 'scrollTo', { value: () => { } });
+// Object.defineProperty(window, 'localStorage', { value: storageMock() });
+// Object.defineProperty(window, 'sessionStorage', { value: storageMock() });
+// Object.defineProperty(window, 'scrollTo', { value: () => { } });
 
-Object.defineProperty(window, 'matchMedia', {
-  value: () => ({
-    matches: false,
-    addListener: () => { },
-    removeListener: () => { }
-  })
-});
+// Object.defineProperty(window, 'matchMedia', {
+//   value: () => ({
+//     matches: false,
+//     addListener: () => { },
+//     removeListener: () => { }
+//   })
+// });
